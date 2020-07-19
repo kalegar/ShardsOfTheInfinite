970cd70014dca031bc9ee42c03ed7ca07ab094f5
kalegar <sanjy33@hotmail.com>
2020-07-18 23:08:52 -0600

    Fresh project

---
+++ desktop/src/com/kalegar/soti/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.kalegar.soti.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kalegar.soti.ShardsOfTheInfinite;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 960;
+		config.height = 540;
+		new LwjglApplication(new ShardsOfTheInfinite(), config);
+	}
+}
